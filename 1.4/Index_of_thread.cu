de85d9a81225d5a318ed1c2e4e6e20a4c14782e9
zeyuanyin <yinzeyuan1037@gmail.com>
2023-07-24 15:53:04 +0800

    add Dim at 1.4

--- 1.4/Index_of_thread.cu
+++ 1.4/Index_of_thread.cu
@@ -5,11 +5,20 @@ __global__ void hello_from_gpu()
     int bid = blockIdx.x;
     int gid = threadIdx.x;
     printf("Hello World from the GPU! blockIdx is %d, threadIdx is %d!\n", bid, gid);
+    
+    
+    //printf("There are %d threads in this block in x direction\n", blockDim.x);
+    //printf("There are %d threads in this block in y direction\n", blockDim.y);
+    
+    //printf("There are %d blocks in this grid in x direction\n", gridDim.x);
+    //printf("There are %d blocks in this grid in y direction\n", gridDim.y);
 }
 
 int main(void)
 {
-    hello_from_gpu<<<2, 4>>>();
+    hello_from_gpu<<<5, 33>>>();
+    
+    
     cudaDeviceSynchronize();
     return 0;
 }
